dc6cca411f422e66e0dc8ac1735149acddf1a1af
Kelvin Brito <britok30@gmail.com>
2021-01-06 00:05:23 -0500

    typescript practice with react

---
+++ src/components/NewTodo.tsx
@@ -0,0 +1,31 @@
+import React, { useRef } from 'react';
+import './NewTodo.css';
+// interface TodoProps {
+//     onAddTodo: (todoText: string) => void;
+// }
+
+type TodoProps = {
+    onAddTodo: (todoText: string) => void;
+};
+
+const NewTodo: React.FC<TodoProps> = (props) => {
+    const textInputRef = useRef<HTMLInputElement>(null);
+
+    const todoSubmitHandler = (e: React.FormEvent) => {
+        e.preventDefault();
+
+        const enteredText = textInputRef.current!.value;
+        props.onAddTodo(enteredText);
+    };
+    return (
+        <form onSubmit={todoSubmitHandler}>
+            <div className="form-control">
+                <label htmlFor="todo-text">Todo Text</label>
+                <input type="text" id="todo-text" ref={textInputRef} />
+            </div>
+            <button type="submit">ADD TODO</button>
+        </form>
+    );
+};
+
+export default NewTodo;
